c5a80fddc999f650dd70a678caa2d0d7b152580c
Mohamed Elmezughi <79088999+M7MD1315@users.noreply.github.com>
2025-05-13 16:19:35 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AccuraKYC",
-            path: "./AccuraOCR.framework"
+            url: "https://github.com/M7MD1315/KYC-SDK-iOS/releases/download/v1.0.0/AccuraOCR.xcframework.zip",
+            checksum: "c1bfcacd64daf12a0b1c6a6c1353cbdefac10114c163178bdc990992dfe5e3c9"
         )
     ]
 )
