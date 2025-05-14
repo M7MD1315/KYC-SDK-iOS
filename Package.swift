af419f887c5c9a3c6f04a7ccacd1c07e919b7aef
Mohamed Elmezughi <79088999+M7MD1315@users.noreply.github.com>
2025-05-14 10:45:25 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,23 @@
-// swift-tools-version:5.5
-import PackageDescription
-
+// Package.swift for Wrapper
 let package = Package(
     name: "AccuraKYC",
-    platforms: [
-        .iOS(.v12)
-    ],
+    platforms: [.iOS(.v12)],
     products: [
-        .library(
-            name: "AccuraKYC",
-            targets: ["AccuraKYC"]
-        )
+        .library(name: "AccuraKYC", targets: ["AccuraKYC"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AFNetworking/AFNetworking", from: "4.0.1"),
+        .package(url: "https://github.com/SVProgressHUD/SVProgressHUD", from: "2.3.1"),
     ],
     targets: [
+        .target(
+            name: "AccuraKYC",
+            dependencies: [
+                "AccuraKYC", 
+                "AFNetworking", 
+                "SVProgressHUD"
+            ]
+        ),
         .binaryTarget(
             name: "AccuraKYC",
             url: "https://github.com/M7MD1315/KYC-SDK-iOS/releases/download/v1.0.0/AccuraOCR.xcframework.zip",
