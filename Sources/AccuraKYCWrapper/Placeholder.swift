04d4784d12a67f202b5940053d2efbafd14e4817
Mohamed Elmezughi <79088999+M7MD1315@users.noreply.github.com>
2025-05-18 16:41:47 +0300

    idk

---
+++ Sources/AccuraKYCWrapper/Placeholder.swift
@@ -0,0 +1,9 @@
+//
+//  Placeholder.swift
+//  accuraocrSDK
+//
+//  Created by Mohamed Elmezughi on 18/05/2025.
+//  Copyright © 2025 Technozer. All rights reserved.
+//
+
+public struct AccuraKYCWrapper {}
